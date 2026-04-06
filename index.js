8dd74df2ef4011ce6d3f3b941684fabf421b04a7
Camilo Castellanos <kml107@hotmail.com>
2026-04-06 12:34:41 -0500

    adds js logic

---
+++ index.js
@@ -0,0 +1,37 @@
+class Caracter {
+    constructor(nombre, palabra, imagen, activo) {
+        this.nombre = nombre;
+        this.palabra = palabra;
+        this.imagen = imagen;
+        this.activo = activo;
+    }
+}
+
+const aLetter = new Caracter("A", "A", "https://upload.wikimedia.org/wikipedia/commons/thumb/8/8e/Letter_A.svg/1200px-Letter_A.svg.png", true);
+const bLetter = new Caracter("B", "B", "https://upload.wikimedia.org/wikipedia/commons/thumb/8/8e/Letter_B.svg/1200px-Letter_B.svg.png", true);
+const cLetter = new Caracter("C", "C", "https://upload.wikimedia.org/wikipedia/commons/thumb/8/8e/Letter_C.svg/1200px-Letter_C.svg.png", true);
+const dLetter = new Caracter("D", "D", "https://upload.wikimedia.org/wikipedia/commons/thumb/8/8e/Letter_D.svg/1200px-Letter_D.svg.png", true);
+const eLetter = new Caracter("E", "E", "https://upload.wikimedia.org/wikipedia/commons/thumb/8/8e/Letter_E.svg/1200px-Letter_E.svg.png", true);
+const fLetter = new Caracter("F", "F", "https://upload.wikimedia.org/wikipedia/commons/thumb/8/8e/Letter_F.svg/1200px-Letter_F.svg.png", true);
+const gLetter = new Caracter("G", "G", "https://upload.wikimedia.org/wikipedia/commons/thumb/8/8e/Letter_G.svg/1200px-Letter_G.svg.png", true);
+const hLetter = new Caracter("H", "H", "https://upload.wikimedia.org/wikipedia/commons/thumb/8/8e/Letter_H.svg/1200px-Letter_H.svg.png", true);
+const iLetter = new Caracter("I", "I", "https://upload.wikimedia.org/wikipedia/commons/thumb/8/8e/Letter_I.svg/1200px-Letter_I.svg.png", true);
+const jLetter = new Caracter("J", "J", "https://upload.wikimedia.org/wikipedia/commons/thumb/8/8e/Letter_J.svg/1200px-Letter_J.svg.png", true);
+const kLetter = new Caracter("K", "K", "https://upload.wikimedia.org/wikipedia/commons/thumb/8/8e/Letter_K.svg/1200px-Letter_K.svg.png", true);
+const lLetter = new Caracter("L", "L", "https://upload.wikimedia.org/wikipedia/commons/thumb/8/8e/Letter_L.svg/1200px-Letter_L.svg.png", true);
+const mLetter = new Caracter("M", "M", "https://upload.wikimedia.org/wikipedia/commons/thumb/8/8e/Letter_M.svg/1200px-Letter_M.svg.png", true);
+const nLetter = new Caracter("N", "N", "https://upload.wikimedia.org/wikipedia/commons/thumb/8/8e/Letter_N.svg/1200px-Letter_N.svg.png", true);
+const oLetter = new Caracter("O", "O", "https://upload.wikimedia.org/wikipedia/commons/thumb/8/8e/Letter_O.svg/1200px-Letter_O.svg.png", true);
+const pLetter = new Caracter("P", "P", "https://upload.wikimedia.org/wikipedia/commons/thumb/8/8e/Letter_P.svg/1200px-Letter_P.svg.png", true);
+const qLetter = new Caracter("Q", "Q", "https://upload.wikimedia.org/wikipedia/commons/thumb/8/8e/Letter_Q.svg/1200px-Letter_Q.svg.png", true);
+const rLetter = new Caracter("R", "R", "https://upload.wikimedia.org/wikipedia/commons/thumb/8/8e/Letter_R.svg/1200px-Letter_R.svg.png", true);
+const sLetter = new Caracter("S", "S", "https://upload.wikimedia.org/wikipedia/commons/thumb/8/8e/Letter_S.svg/1200px-Letter_S.svg.png", true);
+const tLetter = new Caracter("T", "T", "https://upload.wikimedia.org/wikipedia/commons/thumb/8/8e/Letter_T.svg/1200px-Letter_T.svg.png", true);
+const uLetter = new Caracter("U", "U", "https://upload.wikimedia.org/wikipedia/commons/thumb/8/8e/Letter_U.svg/1200px-Letter_U.svg.png", true);
+const vLetter = new Caracter("V", "V", "https://upload.wikimedia.org/wikipedia/commons/thumb/8/8e/Letter_V.svg/1200px-Letter_V.svg.png", true);
+const wLetter = new Caracter("W", "W", "https://upload.wikimedia.org/wikipedia/commons/thumb/8/8e/Letter_W.svg/1200px-Letter_W.svg.png", true);
+const xLetter = new Caracter("X", "X", "https://upload.wikimedia.org/wikipedia/commons/thumb/8/8e/Letter_X.svg/1200px-Letter_X.svg.png", true);
+const yLetter = new Caracter("Y", "Y", "https://upload.wikimedia.org/wikipedia/commons/thumb/8/8e/Letter_Y.svg/1200px-Letter_Y.svg.png", true);
+const zLetter = new Caracter("Z", "Z", "https://upload.wikimedia.org/wikipedia/commons/thumb/8/8e/Letter_Z.svg/1200px-Letter_Z.svg.png", true);
+
+const letras = [aLetter, bLetter, cLetter, dLetter, eLetter, fLetter, gLetter, hLetter, iLetter, jLetter, kLetter, lLetter, mLetter, nLetter, oLetter, pLetter, qLetter, rLetter, sLetter, tLetter, uLetter, vLetter, wLetter, xLetter, yLetter, zLetter];
